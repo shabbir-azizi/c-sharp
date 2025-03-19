5db9b68d5876370138041f157c079cb43828b3e5
Shabbir <azizishabbir355@gmail.com>
2025-03-19 22:52:12 +0500

    new in c sharp and learn c sharp code

--- ttt.cs
+++ ttt.cs
@@ -5,6 +5,50 @@
 
 
 
+
+// Main function with menu
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\nStudent Management System\n");
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Delete Student\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+
 namepace hello
 {
     o references
@@ -156,46 +200,7 @@ void deleteStudent() {
             return;
         }
     }
-
     if (!found) {
         printf("Student not found!\n");
     }
-}
-
-// Main function with menu
-int main() {
-    int choice;
-
-    while (1) {
-        printf("\nStudent Management System\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Delete Student\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addStudent();
-                break;
-            case 2:
-                displayStudents();
-                break;
-            case 3:
-                searchStudent();
-                break;
-            case 4:
-                deleteStudent();
-                break;
-            case 5:
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    }
-
-    return 0;
 }
