cb6741381dadc73b0067ae75551d42270d1f8131
Shabbir <azizishabbir355@gmail.com>
2025-03-22 18:51:16 +0500

    learn c and code about function to add structer to store

--- ttt.cs
+++ ttt.cs
@@ -118,35 +118,6 @@ int main() {
 
 #define MAX_STUDENTS 100
 
-// Structure to store student details
-typedef struct {
-    int id;
-    char name[50];
-    float marks;
-} Student;
-
-Student students[MAX_STUDENTS];
-int studentCount = 0;
-
-// Function to add a student
-void addStudent() {
-    if (studentCount >= MAX_STUDENTS) {
-        printf("Student limit reached!\n");
-        return;
-    }
-
-    printf("Enter Student ID: ");
-    scanf("%d", &students[studentCount].id);
-    printf("Enter Student Name: ");
-    scanf(" %[^\n]", students[studentCount].name);
-    printf("Enter Student Marks: ");
-    scanf("%f", &students[studentCount].marks);
-
-    studentCount++;
-    printf("Student added successfully!\n");
-}
-
-// Function to display all students
 void displayStudents() {
     if (studentCount == 0) {
         printf("No students to display.\n");
@@ -326,3 +297,39 @@ int main() {
 #include <string.h>
 
 #define MAX_STUDENTS 100
+
+
+
+
+typedef struct {
+    int id;
+    char name[50];
+    float marks;
+} Student;
+
+Student students[MAX_STUDENTS];
+int studentCount = 0;
+
+// Function to add a student
+void addStudent() {
+    if (studentCount >= MAX_STUDENTS) {
+        printf("Student limit reached!\n");
+        return;
+    }
+
+    printf("Enter Student ID: ");
+    scanf("%d", &students[studentCount].id);
+    printf("Enter Student Name: ");
+    scanf(" %[^\n]", students[studentCount].name);
+    printf("Enter Student Marks: ");
+    scanf("%f", &students[studentCount].marks);
+
+    studentCount++;
+    printf("Student added successfully!\n");
+}
+
+
+
+
+
+
