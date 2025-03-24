bc49bd6d6a227c98c415def94b75f4513f1920e9
Shabbir <azizishabbir355@gmail.com>
2025-03-24 20:30:55 +0500

    learn c and code about main function  with menue

--- ttt.cs
+++ ttt.cs
@@ -6,62 +6,6 @@
 
 
 
-// Main function with menu
-int main() {
-    int choice;
-
-    while (1) {
-        printf("\nStudent Management System\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Delete Student\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addStudent();
-                break;
-            case 2:
-                displayStudents();
-                break;
-            case 3:
-                searchStudent();
-                break;
-            case 4:
-                deleteStudent();
-                break;
-            case 5:
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    }
-
-    return 0;
-}
-
-
-namepace hello
-{
-    o references
-    class Program
-    {
-        o references
-        static void main ( straing[]args)
-        {
-            console.WriteLine("Hellow world");
-            console.readline();
-        }
-
-    }
-}
-
-
-
 
 
 
@@ -369,4 +313,74 @@ void deleteStudent() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\nStudent Management System\n");
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Delete Student\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}
+
+
+namepace hello
+{
+    o references
+    class Program
+    {
+        o references
+        static void main ( straing[]args)
+        {
+            console.WriteLine("Hellow world");
+            console.readline();
+        }
+
+    }
+}
 
