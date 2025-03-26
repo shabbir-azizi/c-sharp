6642923e01b9d08aa97714e2ccac987c3990d181
Shabbir <azizishabbir355@gmail.com>
2025-03-26 20:09:24 +0500

    learn c and code about function to struct function to search function to delete

--- ttt.cs
+++ ttt.cs
@@ -367,6 +367,72 @@ int studentCount = 0;
 
 
 
+
+
+
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\nStudent Management System\n");
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Delete Student\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}
+
+
+namepace hello
+{
+    o references
+    class Program
+    {
+        o references
+        static void main ( straing[]args)
+        {
+            console.WriteLine("Hellow world");
+            console.readline();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
 int main() {
     int choice;
 
