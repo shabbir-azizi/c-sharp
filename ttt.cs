c2041d31b0741a4d5f70044a748920f5a99e44da
Shabbir <azizishabbir355@gmail.com>
2025-03-29 22:38:19 +0500

    learn c and code about consol.writrline and consol.clear and static word

--- ttt.cs
+++ ttt.cs
@@ -525,4 +525,48 @@ class DigitalClock
 }
 
 
+
+
+
+
+
+
+
+using System;
+using System.Threading;
+
+class DigitalClock
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear(); // Clear the console to update the clock display
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
+            Thread.Sleep(1000); // Wait for 1 second before updating
+        }
+    }
+}
+
  
+
+
+
+namepace hello
+{
+    o references
+    class Program
+    {
+        o references
+        static void main ( straing[]args)
+        {
+            console.WriteLine("Hellow world");
+            console.readline();
+        }
+
+    }
+}
+
+
+
+
