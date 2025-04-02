9a0128fc26d753f7f07fcbda06bcb349772ee188
Shabbir <azizishabbir355@gmail.com>
2025-04-02 16:27:45 +0000

    learn c and code about consol.writrline and consol.clear and static word

--- gg.cs
+++ gg.cs
@@ -209,3 +209,40 @@ class DigitalClock
         }
     }
 }
+
+
+
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\nStudent Management System\n");
+        printf("1. Add Student\n");
+        printf("2. Display Students\n");
+        printf("3. Search Student\n");
+        printf("4. Delete Student\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
