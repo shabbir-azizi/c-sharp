085fff66d963c357f85a9311eb5ca01c998fff90
Shabbir <azizishabbir355@gmail.com>
2025-04-06 15:52:15 +0500

    learn c and  new code make a projrct about digital clock

--- ttt.cs
+++ ttt.cs
@@ -433,39 +433,7 @@ namepace hello
 
 
 
-int main() {
-    int choice;
 
-    while (1) {
-        printf("\nStudent Management System\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Delete Student\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addStudent();
-                break;
-            case 2:
-                displayStudents();
-                break;
-            case 3:
-                searchStudent();
-                break;
-            case 4:
-                deleteStudent();
-                break;
-            case 5:
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    }
 
     return 0;
 }
@@ -508,45 +476,12 @@ class DigitalClock
 
 
 
-using System;
-using System.Threading;
-
-class DigitalClock
-{
-    static void Main()
-    {
-        while (true)
-        {
-            Console.Clear(); // Clear the console to update the clock display
-            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
-            Thread.Sleep(1000); // Wait for 1 second before updating
-        }
-    }
-}
 
 
 
 
 
 
-
-
-
-using System;
-using System.Threading;
-
-class DigitalClock
-{
-    static void Main()
-    {
-        while (true)
-        {
-            Console.Clear(); // Clear the console to update the clock display
-            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
-            Thread.Sleep(1000); // Wait for 1 second before updating
-        }
-    }
-}
 
  
 
