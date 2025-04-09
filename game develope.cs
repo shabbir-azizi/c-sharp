f7ead0446198cfca15d1ae164e32b778753820a0
Shabbir <azizishabbir355@gmail.com>
2025-04-09 21:56:21 +0500

    learn c and  new code project about game in c# programing 5 game

---
+++ game develope.cs
@@ -0,0 +1,35 @@
+
+
+
+note  prject about game in c# programing language 
+5 game code 
+
+
+
+
+
+
+SECTION 1:
+introduction to C# programing language & unity  
+
+
+SECTION 2:
+frist game development named obstacle avoiding game 
+
+
+SECTION 3:
+scond game development named rocket booster game 
+
+
+SECTION 4:
+third game development named jet fighter game 
+
+
+SECTION 5:
+fourth game development named realm rush game 
+
+
+SECTION 6:
+
+
+last game development named frist person shoter game 
