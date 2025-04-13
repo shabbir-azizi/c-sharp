fef39974086fa5c146d6af96859238f8e065e29b
Shabbir <azizishabbir355@gmail.com>
2025-04-13 17:02:13 +0500

    learn c and  new code

---
+++ tdt.cs
@@ -0,0 +1,18 @@
+using system;
+using system.Collections.Generic;
+using system.linq;
+using system.Text;
+using system.Threading.Tasks;
+
+namespace Hello
+{
+
+    class program
+    {
+
+        static void Main (string []args)
+        {    
+        }
+    }
+}
+
