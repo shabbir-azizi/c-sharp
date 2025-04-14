dcc1d8fa0aaea9229b640dd36f579d7ebf7f8728
Shabbir <azizishabbir355@gmail.com>
2025-04-14 21:02:04 +0500

    learn c and  new code sbout boolean , interger ,console write line,consol read line ,floating,variable

--- tdt.cs
+++ tdt.cs
@@ -12,7 +12,24 @@ namespace Hello
 
         static void Main (string []args)
         {    
+            // Thise is importent for debugging. do note remove
+
+            int shabbir = 19;//integer variable 
+            /* data type in c#:
+            integer - int shabbir ;
+            floating point number - float that 
+            character - char a = 'A';
+            boolean -
+              */
+            Console.WriteLine( "hellow world ");
+            Console.Write( "hellow shabbir ");
+            Console.WriteLine( "i like programing ");
+            Console.WriteLine( "i like programing and code" + harry);
+         
+
+            console.readline();
         }
     }
 }
+
 
