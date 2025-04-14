7ebb8db02809fec0f9418d168b358f5d33ef3cda
Shabbir <azizishabbir355@gmail.com>
2025-04-14 21:37:28 +0500

    learn c and  new code sbout boolean , interger ,console write line,consol read line ,floating,variable

--- ttt.cs
+++ ttt.cs
@@ -524,44 +524,6 @@ class DigitalClock
 
 
 
-int main() {
-    int choice;
-
-    while (1) {
-        printf("\nStudent Management System\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Delete Student\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addStudent();
-                break;
-            case 2:
-                displayStudents();
-                break;
-            case 3:
-                searchStudent();
-                break;
-            case 4:
-                deleteStudent();
-                break;
-            case 5:
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    }
-    using System;
-using System.Drawing;
-using System.Windows.Forms;
-
-public class DigitalClock : Form
 {
     private Label timeLabel;
     private Timer timer;
@@ -605,23 +567,6 @@ public class DigitalClock : Form
 }
 
 
-// afsaf
-// sfaf
-using System;
-using System.Threading;
-
-class DigitalClock
-{
-    static void Main()
-    {
-        while (true)
-        {
-            Console.Clear(); // Clear the console to update the clock display
-            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
-            Thread.Sleep(1000); // Wait for 1 second before updating
-        }
-    }
-}
 
 
 using System;
@@ -639,6 +584,25 @@ class DigitalClock
         }
     }
 }
+
+
+using System;
+using System.Threading;
+
+class DigitalClock
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear(); // Clear the console to update the clock display
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
+            Thread.Sleep(1000); // Wait for 1 second before updating
+        }
+    }
+}
+
+
 
 
 
