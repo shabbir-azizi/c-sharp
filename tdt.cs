1b830e9667dd45be81e23e39fd48101a123d6f23
Shabbir <azizishabbir355@gmail.com>
2025-04-21 11:38:28 +0500

    learn c and  new code sbout boolean , interger ,console write line,consol read line ,floating,variable

--- tdt.cs
+++ tdt.cs
@@ -30,18 +30,18 @@ namespace Hello
          
 
 
-class DigitalClock
-{
-    static void Main()
-    {
-        while (true)
-        {
-            Console.Clear(); // Clear the console to update the clock display
-            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
-            Thread.Sleep(1000); // Wait for 1 second before updating
-        }
-    }
-}
+// class DigitalClock
+// {
+//     static void Main()
+//     {
+//         while (true)
+//         {
+//             Console.Clear(); // Clear the console to update the clock display
+//             Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
+//             Thread.Sleep(1000); // Wait for 1 second before updating
+//         }
+//     }
+// }
 
 
             /* data type in c#:
