44f77fd20eee6af112cbc87a66ff3d263d85f98b
Shabbir <azizishabbir355@gmail.com>
2025-04-30 14:54:17 +0500

    learn c and  practise c sharp

--- aaa.cs
+++ aaa.cs
@@ -9,25 +9,25 @@
 //     abstract class Animal
     {
         // Encapsulation: private field with public property
-        private string name;
+        // private string name;
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        // public string Name
+        // {
+        //     get { return name; }
+        //     set { name = value; }
+        // }
 
         // Abstract method (must be overridden)
 
 
-        public abstract void MakeSound();
+    //     public abstract void MakeSound();
 
-        // Concrete method
-        public void DisplayInfo()
-        {
-            Console.WriteLine($"Animal Name: {Name}");
-        }
-    }
+    //     // Concrete method
+    //     public void DisplayInfo()
+    //     {
+    //         Console.WriteLine($"Animal Name: {Name}");
+    //     }
+    // }
 
     // Derived class (Inheritance)
     class Dog : Animal
