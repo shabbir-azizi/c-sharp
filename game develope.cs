98c4338d832ca212f2f8d380940e8b3ffaa3a680
Shabbir <azizishabbir355@gmail.com>
2025-07-03 13:00:54 +0500

    practise and larn new about c language

--- game develope.cs
+++ game develope.cs
@@ -1,72 +1,60 @@
 
 
 
-note  prject about game in c# programing language 
-5 game code 
+// note  prject about game in c# programing language 
+// 5 game code 
 
 
 
-SECTION 1:
-introduction to C# programing language & unity  
+// SECTION 1:
+// introduction to C# programing language & unity  
 
 
-SECTION 2:
-frist game development named obstacle avoiding game 
+// SECTION 2:
+// frist game development named obstacle avoiding game 
 
 
-SECTION 3:
-scond game development named rocket booster game 
+// SECTION 3:
+// scond game development named rocket booster game 
 
 
-SECTION 4:
-third game development named jet fighter game 
+// SECTION 4:
+// third game development named jet fighter game 
 
 
-SECTION 5:
-fourth game development named realm rush game 
+// SECTION 5:
+// fourth game development named realm rush game 
 
 
-SECTION 6:
+// SECTION 6:
 
 
-last game development named frist person shoter game 
-
-
-
-
-
-int main() {
-    printf("hellow pw");
-    printf(\n)
-    printf("hellow lw");
-    return 0;
-    float x = 5
-    float c = 9
-    float r = 3
-    float z = 5 / 2.5;'';
-    printf("%f",z);
-
-    int r = 5;
-    int v = 4 * 3.14 * r * r * r / 3;
-    printf("the volume is : %f",v);
-    }
+// last game development named frist person shoter game 
 
 
 
 
-int main() {
-    int choice;
 
-    while (1) {
-        printf("\nStudent Management System\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Delete Student\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+// int main() {
+//     printf("hellow pw");
+//     printf(\n)
+//     printf("hellow lw");
+//     return 0;
+//     float x = 5
+//     float c = 9
+//     float r = 3
+//     float z = 5 / 2.5;'';
+//     printf("%f",z);
 
+//     int r = 5;
+//     int v = 4 * 3.14 * r * r * r / 3;
+//     printf("the volume is : %f",v);
+//     }
+
+
+
+
+// 
         switch (choice) {
             case 1:
                 addStudent();
@@ -86,23 +74,48 @@ int main() {
             default:
                 printf("Invalid choice! Please try again.\n");
         }
-    }
-    using System;
-using System.Drawing;
-using System.Windows.Forms;
+//     }
+//     using System;
+// using System.Drawing;
+// using System.Windows.Forms;
 
-// public class DigitalClock : Form
+// // public class DigitalClock : Form
 
 
  
-}
+// }
 
-   for (int i = 0; i < studentCount; i++) {
-        if (students[i].id == id) {
-            printf("\nStudent Found!\n");
-            printf("ID: %d\nName: %s\nMarks: %.2f\n", students[i].id, students[i].name, students[i].marks);
-            return;
-        }
-    }
-    printf("Student not found!\n");
-}
+//    for (int i = 0; i < studentCount; i++) {
+//         if (students[i].id == id) {
+//             printf("\nStudent Found!\n");
+//             printf("ID: %d\nName: %s\nMarks: %.2f\n", students[i].id, students[i].name, students[i].marks);
+//             return;
+//         }
+//     }
+//     printf("Student not found!\n");
+// }
+
+
+      // cat.MakeSound();
+
+            // namespace hello    
+            // {
+            // class player 
+            // {
+            //     public string name = "harry";
+            //     public int health = 49;
+
+            //     public int gethealth ()
+            //     {
+            //         return health ;
+            //     }
+
+            //     console.WriteLine(tommy.gethealth());
+            //     console.WriteLine(tommy.health);
+            //     public void sethealth (int h)
+            //     {
+            //         health = h;
+            //     }
+            // }
+
+            
