69aaeac523610cb83f907d7331d908137608ea25
Shabbir <azizishabbir355@gmail.com>
2025-07-15 15:41:03 +0500

    practise and larn new about c language

--- ttt.cs
+++ ttt.cs
@@ -787,3 +787,23 @@ void searchStudent() {
         printf("File not found!\n");
         return;
     }
+
+
+    // using System;
+
+// namespace OOPExample
+// {
+//     // Abstract class (Abstraction)
+//     abstract class Animal
+    // {
+        // Encapsulation: private field with public property
+        // private string name;
+
+        // public string Name
+        // {
+        //     get { return name; }
+        //     set { name = value; }
+        // }
+
+        // Abstract method (must be overridden)
+
