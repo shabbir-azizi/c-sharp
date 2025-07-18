0c1f3e8850c6e54b3364b9ed258f8277930ddc0b
Shabbir <azizishabbir355@gmail.com>
2025-07-18 15:55:15 +0500

    practise and larn new about c language

--- ttt.cs
+++ ttt.cs
@@ -875,3 +875,24 @@ int main() {
     // printf("ID\tName\t\tMarks\n");
     // printf("---------------------------------\n");
 
+
+
+//     studentCount++;
+//     printf("Student added successfully!\n");
+// }
+// / Structure to store student details
+// typedef struct {
+//     int id;
+//     char name[50];
+//     float marks;
+// } Student;
+
+// Student students[MAX_STUDENTS];
+// int studentCount = 0;
+
+// // Function to add a student
+// void addStudent() {
+//     if (studentCount >= MAX_STUDENTS) {
+//         printf("Student limit reached!\n");
+//         return;
+//     }
