2f6c86784395c54c24b2bb6e4b260f06bebf8002
Shabbir <azizishabbir355@gmail.com>
2025-07-19 21:32:37 +0500

    practise and larn new about c language

--- game develope.cs
+++ game develope.cs
@@ -1,60 +1,6 @@
 
 
 
-// note  prject about game in c# programing language 
-// 5 game code 
-
-
-
-// SECTION 1:
-// introduction to C# programing language & unity  
-
-
-// SECTION 2:
-// frist game development named obstacle avoiding game 
-
-
-// SECTION 3:
-// scond game development named rocket booster game 
-
-
-// SECTION 4:
-// third game development named jet fighter game 
-
-
-// SECTION 5:
-// fourth game development named realm rush game 
-
-
-// SECTION 6:
-
-
-// last game development named frist person shoter game 
-
-
-
-
-
-// int main() {
-//     printf("hellow pw");
-//     printf(\n)
-//     printf("hellow lw");
-//     return 0;
-//     float x = 5
-//     float c = 9
-//     float r = 3
-//     float z = 5 / 2.5;'';
-//     printf("%f",z);
-
-//     int r = 5;
-//     int v = 4 * 3.14 * r * r * r / 3;
-//     printf("the volume is : %f",v);
-//     }
-
-
-
-
-// 
         switch (choice) {
             case 1:
                 addStudent();
@@ -73,7 +19,50 @@
                 exit(0);
             default:
                 printf("Invalid choice! Please try again.\n");
+        }        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
         }
+
+
+
+
+
+// 
+
 //     }
 //     using System;
 // using System.Drawing;
@@ -83,39 +72,8 @@
 
 
  
-// }
-
-//    for (int i = 0; i < studentCount; i++) {
-//         if (students[i].id == id) {
-//             printf("\nStudent Found!\n");
-//             printf("ID: %d\nName: %s\nMarks: %.2f\n", students[i].id, students[i].name, students[i].marks);
-//             return;
-//         }
-//     }
-//     printf("Student not found!\n");
-// }
 
 
-      // cat.MakeSound();
 
-            // namespace hello    
-            // {
-            // class player 
-            // {
-            //     public string name = "harry";
-            //     public int health = 49;
 
-            //     public int gethealth ()
-            //     {
-            //         return health ;
-            //     }
 
-            //     console.WriteLine(tommy.gethealth());
-            //     console.WriteLine(tommy.health);
-            //     public void sethealth (int h)
-            //     {
-            //         health = h;
-            //     }
-            // }
-
-            
