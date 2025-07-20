d436f42a14c668f1969771dc0962769a3736391b
Shabbir <azizishabbir355@gmail.com>
2025-07-20 12:19:52 +0500

    practise and larn new about c language

--- ttt.cs
+++ ttt.cs
@@ -960,3 +960,25 @@ printf("Student not found!\n");
 
 // // public class DigitalClock : Form
 
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+
