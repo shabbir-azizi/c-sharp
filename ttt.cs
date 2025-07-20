455beda4a965908cf4c0b326d17b66a1c859d1f6
Shabbir <azizishabbir355@gmail.com>
2025-07-20 12:20:58 +0500

    practise and larn new about c language

--- ttt.cs
+++ ttt.cs
@@ -1003,4 +1003,18 @@ printf("Student not found!\n");
             case 5:
                 printf("Exiting...\n");
                 exit(0);
-    
+           switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+
+
