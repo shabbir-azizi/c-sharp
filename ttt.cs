27cc83a785a74e92978ad17fba83412848c40836
Shabbir <azizishabbir355@gmail.com>
2025-07-23 22:17:21 +0500

    practise and larn new about c language

--- ttt.cs
+++ ttt.cs
@@ -1082,13 +1082,26 @@ namepace hello
 {
     o references
     class Program
-    {
-        o references
-        static void main ( straing[]args);
+{
+    o references
+        static void main(straing[] args);
         {
             console.WriteLine("Hellow world");;
             console.readline();'
         }
 
+    }
+}
+
+class DigitalClock
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear(); // Clear the console to update the clock display
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss")); // Display current time
+            Thread.Sleep(1000); // Wait for 1 second before updating
+        }
     }
 }
