c6a1fc4ffb36b9426be4520a8de3ef239e54d860
Shabbir <azizishabbir355@gmail.com>
2025-07-25 16:29:36 +0500

    practise and larn new about c language

--- tdt.cs
+++ tdt.cs
@@ -261,4 +261,27 @@ namespace Hello
 
 
 
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                displayStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}
 
